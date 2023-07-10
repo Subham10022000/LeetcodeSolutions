d5739856f60c6e56426400f17c20873bfe617f74
Subham Mohapatra <105945953+Subham10022000@users.noreply.github.com>
2023-07-10 19:42:23 +0530

    Update BestTimeToBuyAndSell.java

--- BestTimeToBuyAndSell.java
+++ BestTimeToBuyAndSell.java
@@ -1,1 +1,21 @@
+public class BestTimeToBuyAndSell {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right;
+        int profit=0;
+        if(prices.length==1) return 0; // you can use stack for this its on you
+        for(right=1;right<prices.length;right++){
+            if(prices[left]>prices[right])
+                left=right;
+            else if(prices[left]<prices[right])
+                profit =Math.max(profit,prices[right]-prices[left]);
+        }
+        return profit;
+    }
+    public static void main(String[] args){
+        int arr[]={7,1,5,4,6,2};
+        BestTimeToBuyAndSell b1 = new BestTimeToBuyAndSell();
+       System.out.println( b1.maxProfit(arr));
+    }
+}
 
